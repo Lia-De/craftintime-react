63ec6b3417dfa3c73f53221f35d8b27f4f0f7d40
Jenny Evertsson <jentap@gmail.com>
2025-03-31 11:05:56 +0200

    changing to hashrouter for static pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router'
 import { Layout } from './components/Layout.jsx'
 import { Home } from './pages/Home.jsx'
 import { Project } from './pages/Project.jsx'
 import { Tag } from './pages/Tag.jsx'
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   
   return (
-    
-    <BrowserRouter  basename="/craftintime-react">
+    // <BrowserRouter  basename="/craftintime-react">
+    <HashRouter>
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
@@ -17,7 +18,9 @@ function App() {
         <Route path="/tags" element={<Tag />} />
       </Route>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    // </BrowserRouter>
+    
     
   )
 }
